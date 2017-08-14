08278583dc205cce8a4146677e1550dbac31fc80
Master Web Dev Chris <frewin.christopher@gmail.com>
2017-08-14 11:07:48 +0200

    server.js much cleaner now, finally satisfied. may need to add regex for more complex tracker1 and tracker2. as soon as there is a space the stream tries to track the exact word

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.get('/', function (req,res) {
 	res.sendFile(path.join(__dirname + '/dist/index.html')); // serve our static index.html
 });
 http.listen(port, function() {
-  console.log('listening on *:' + port);
+  console.log('listening on port ' + port);
 });
 
 // stream the tweets from the bot; emit them with socket.io
-var tracker1 = ':)',
-    tracker2 = ':(';
+var tracker1 = 'obama',
+    tracker2 = 'trump';
 
 var bot = new Twit({
   consumer_key: process.env.EGGHEAD_TWITTER_BOT_CONSUMER_KEY,
@@ -32,27 +32,19 @@ var bot = new Twit({
   access_token_secret: process.env.EGGHEAD_TWITTER_BOT_CONSUMER_ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000
 });
-var stream = bot.stream('statuses/filter', {track: [tracker1, tracker2]}).on('tweet', function(tweet) {
-	console.log(tweet.text);
-if (tweet.text.includes('RT @')) {
-	return; // we don't want retweets, just originals
-}
-if (tweet.text.includes(tracker1) && tweet.text.includes(tracker2)) {
-	io.emit('tracker1', tweet.text + '\n');
-	io.emit('tracker2', tweet.text + '\n');
-} else if (tweet.text.includes(tracker1)) {
-	io.emit('tracker1', tweet.text + '\n');
-} else if (tweet.text.includes(tracker2)) {
-	io.emit('tracker2', tweet.text + '\n');
-}});
+var stream;
+
+
 
 // need to register the socket.on in here, otherwise it wont' work
 io.sockets.on('connection', function (socket) {
-		io.emit('initialTrackers', {tracker1: tracker1, tracker2: tracker2} );
-		socket.on('updateTrackers', function(trackers) {
-			stream.stop();
-			stream = bot.stream('statuses/filter', {track: [trackers.tracker1, trackers.tracker2]}).on('tweet', function(tweet) {
-				console.log(tweet.text);
+
+		// TODO: global count number that is checked every 4 seconds - if it is 0, send event that creates temp message front extends
+		// TODO: add functionality for geo location - can see what parts of the world are tweeting what! :)
+		function track(tweet, tracker1, tracker2) {
+			console.log(tracker1);
+			console.log(tracker2);
+
 			if (tweet.text.includes('RT @')) {
 				console.log("is retweet, returning...")
 				return; // we don't want retweets, just originals
@@ -67,6 +59,19 @@ io.sockets.on('connection', function (socket) {
 			} else if (tweet.text.includes(tracker2)) {
 				console.log("emitting just tracker2");
 				io.emit('tracker2', tweet.text + '\n');
-			}});
+			}
+		}
+
+		io.emit('initialTrackers', {tracker1: tracker1, tracker2: tracker2} );
+
+		stream = bot.stream('statuses/filter', {track: [tracker1, tracker2]})
+								.on('tweet', (tweet) => track(tweet, tracker1, tracker2));
+
+		socket.on('updateTrackers', function(trackers) {
+			tracker1 = trackers.tracker1;
+			tracker2 = trackers.tracker2;
+			stream.stop();
+			stream = bot.stream('statuses/filter', {track: [tracker1, tracker2]})
+									.on('tweet', (tweet) => track(tweet,tracker1,tracker2));
 		});
 });
